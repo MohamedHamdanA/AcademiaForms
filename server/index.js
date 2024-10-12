613c962b41b3766de20dccd486e4a42dfe22709a
MohamedHamdanA <149225179+MohamedHamdanA@users.noreply.github.com>
2024-10-12 14:43:59 +0530

    minor change

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 mongoose.connect(
-  "mongo : uri"
+  process.env.MONGODB_URI
 );
 
 app.use(cors());
@@ -26,7 +26,7 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("connection established successfully");
 });
-
-app.listen(5000, () => {
+const port = process.env.PORT || 5000
+app.listen(port, () => {
   console.log("Server is running on port 5000");
 });
